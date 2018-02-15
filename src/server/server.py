8bbf62588b5dec428422ef43dec0ed157866c5c8
ZubAnt <ya.zubarevanton@yandex.ru>
2018-02-16 00:01:26 +0300

    run empty Listener

--- src/server/server.py
+++ src/server/server.py
@@ -2,15 +2,18 @@ from asyncio import AbstractEventLoop
 
 from injector import singleton
 
+from services.listener import Listener
+
 
 @singleton
 class Server(object):
 
     def __init__(self, loop: AbstractEventLoop):
         self._loop = loop
+        self._listener = Listener()
 
     def start(self):
-        pass
+        self._loop.create_task(self._listener.start())
 
     def stop(self):
-        pass
+        self._listener.stop()
