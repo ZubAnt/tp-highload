3eaf5078694278fb8f5c2b6ed090bdd3e0d88bb4
ZubAnt <ya.zubarevanton@yandex.ru>
2018-02-24 16:35:56 +0300

    add content-types

---
+++ src/server/models/content_types.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class ContentTypes(Enum):
+
+    html = 'text/html'
+    css = 'text/css'
+    js = 'text/javascript'
+    jpg = 'image/jpeg'
+    jpeg = 'image/jpeg'
+    png = 'image/png'
+    swf = 'application/x-shockwave-flash'
+    gif = 'image/gif'
+    txt = 'text/txt'
