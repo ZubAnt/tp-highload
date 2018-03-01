7d5ed90f6fbfaac59cc8f1794e67b13bc949a4fc
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-01 15:48:26 +0300

    commit

--- src/server/services/file_reader.py
+++ src/server/services/file_reader.py
@@ -14,34 +14,48 @@ class FileReader(object):
     def __init__(self, conf: Configure, loop: AbstractEventLoop):
         self._conf = conf
         self._loop = loop
-        self._executor = ThreadPoolExecutor()
+        self._executor = ThreadPoolExecutor(max_workers=4)
 
     async def read(self, filename: str) -> bytes:
-        data: bytes = b""
-        file = open(filename, 'rb')
-        fd = file.fileno()
-        flag = fcntl(fd, F_GETFL)
-        fcntl(fd, F_SETFL, flag | os.O_NONBLOCK)
-        flag = fcntl(fd, F_GETFL)
-        if flag & os.O_NONBLOCK:
-            logging.info(f"[FileReader] flag & os.O_NONBLOCK")
-        else:
-            logging.info(f"[FileReader] not")
+        # data: bytes = b""
+        # file = open(filename, 'rb')
+        # fd = file.fileno()
+        # flag = fcntl(fd, F_GETFL)
+        # fcntl(fd, F_SETFL, flag | os.O_NONBLOCK)
+        # flag = fcntl(fd, F_GETFL)
+        # if flag & os.O_NONBLOCK:
+        #     logging.info(f"[FileReader] flag & os.O_NONBLOCK")
+        # else:
+        #     logging.info(f"[FileReader] not")
+        #
+        # logging.info(f"[FileReader] try read {filename}")
+        # logging.info(f"[FileReader] loop status: {self._loop.is_running()}")
+        # while True:
+        #     logging.debug(f"[FileReader] try read...")
+        #     #
+        #     chunk = await self._loop.run_in_executor(self._executor, file.read, self._conf.read_chunk_size)
+        #     # chunk = await self._read(file, 32)
+        #     # chunk = self._executor.submit(self._read_sync_chunk, file, 64)
+        #     logging.debug(f"[FileReader] chunk: {chunk}")
+        #     await sleep(0)
+        #     if chunk == b'':
+        #         break
+        #     data += chunk
+        # file.close()
+        # return data
+        return await self._loop.run_in_executor(self._executor, self._open_and_read, filename)
 
-        logging.info(f"[FileReader] try read {filename}")
-        logging.info(f"[FileReader] loop status: {self._loop.is_running()}")
-        while True:
-            logging.debug(f"[FileReader] try read...")
-            #
-            chunk = await self._loop.run_in_executor(self._executor, file.read, self._conf.read_chunk_size)
-            # chunk = await self._read(file, 32)
-            # chunk = self._executor.submit(self._read_sync_chunk, file, 64)
-            logging.debug(f"[FileReader] chunk: {chunk}")
-            await sleep(0)
-            if chunk == b'':
-                break
-            data += chunk
-        file.close()
+    def _open_and_read(self, filename: str) -> bytes:
+
+        data: bytes = b""
+        with open(filename, 'rb') as file:
+            while True:
+                logging.debug(f"[FileReader] try read chunk...")
+                chunk = self._read_chunk_sync(file, self._conf.read_chunk_size)
+                data += chunk
+                logging.debug(f"[FileReader] chunk: {chunk}")
+                if chunk == b'':
+                    break
         return data
 
     @asyncio.coroutine
@@ -49,7 +63,7 @@ class FileReader(object):
         yield from file.read(chunk_size)
 
     @classmethod
-    def _read_sync_chunk(cls, file, chunk_size):
+    def _read_chunk_sync(cls, file, chunk_size):
         return file.read(chunk_size)
 
 
