d3a9af770f7fb4e95ec4e04146ad65f04004bfb5
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-02 18:36:17 +0300

    fix RequestExecutor

--- src/server/models/resource.py
+++ src/server/models/resource.py
@@ -3,9 +3,9 @@ from models.content_types import ContentTypes
 
 class Resource(object):
 
-    def __init__(self, filename: str = None, file_url: str = None, content_type: ContentTypes = None) -> None:
+    def __init__(self, filename: str = None, file_path: str = None, content_type: ContentTypes = None) -> None:
         self._filename = filename
-        self._file_url = file_url
+        self._file_path = file_path
         self._content_type = content_type
 
     @property
@@ -13,8 +13,8 @@ class Resource(object):
         return self._filename
 
     @property
-    def file_url(self) -> str:
-        return self._file_url
+    def file_path(self) -> str:
+        return self._file_path
 
     @property
     def content_type(self) -> ContentTypes:
